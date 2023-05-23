ffda86e29655e5c82abed6e9ba452178369ff9f6
Dinhnguyen1812002@gmail.com <Dinhnguyen1812002@gmail.com>
2023-05-23 17:24:18 +0700

    validate dữ liệu và hàm cập nhập

--- src/main/java/CrudApi/CrudBook/controller/BookController.java
+++ src/main/java/CrudApi/CrudBook/controller/BookController.java
@@ -4,13 +4,15 @@ import CrudApi.CrudBook.entity.Book;
 import CrudApi.CrudBook.exception.NotFoundException;
 import CrudApi.CrudBook.repository.IBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-
+@CrossOrigin("*")
 @RequestMapping("/api/v1/books")
 public class BookController {
     @Autowired
@@ -28,25 +30,33 @@ public class BookController {
         return bookRepository.save(book);
     }
     //
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable Long id){
-        Book book = bookRepository.findById(id).
-                orElseThrow(() -> new NotFoundException("Không tìm thấy mã sách: "+id));
+        Book book = bookRepository.findById(id).orElseThrow(() -> new NotFoundException("Không tìm thấy mã sách: "+id));
         return ResponseEntity.ok(book);
     }
     //upadte chỉnh sủa 1 quyển sách
-    @PutMapping("{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id,@RequestBody Book bookDetail){
-        Book updateBook = bookRepository.findById(id).
-                orElseThrow(() -> new NotFoundException("Không tìm thấy mã sách: "+id));
-        updateBook.setTitle(bookDetail.getTitle());
-        updateBook.setAuthor(bookDetail.getAuthor());
-        updateBook.setPrice(bookDetail.getPrice());
-        updateBook.setImage(bookDetail.getImage());
-        bookRepository.save(updateBook);
-        return ResponseEntity.ok(updateBook);
+//    @PutMapping("{id}")
+//    public ResponseEntity<Book> updateBook(@PathVariable Long id,@RequestBody Book bookDetail){
+//        Book updateBook = bookRepository.findById(id).
+//                orElseThrow(() -> new NotFoundException("Không tìm thấy mã sách: "+id));
+//        updateBook.setTitle(bookDetail.getTitle());
+//        updateBook.setAuthor(bookDetail.getAuthor());
+//        updateBook.setPrice(bookDetail.getPrice());
+//        updateBook.setImage(bookDetail.getImage());
+//        bookRepository.save(updateBook);
+//        return ResponseEntity.ok(updateBook);
+//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> update(@PathVariable Long id,@RequestBody Book book )
+    {
+        Optional<Book> bookUpdate = bookRepository.findById(id);
+        return  bookUpdate.map(book1->{
+                book.setId(book1.getId());
+                return new ResponseEntity<>(bookRepository.save(book), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Book> deleteBook(@PathVariable Long id)
     {
         Book deleteBook = bookRepository.findById(id).
--- src/main/java/CrudApi/CrudBook/entity/Book.java
+++ src/main/java/CrudApi/CrudBook/entity/Book.java
@@ -1,6 +1,9 @@
 package CrudApi.CrudBook.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +18,19 @@ public class Book {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long Id;
     @Column(name = "Title")
+    @NotEmpty(message = "Không để trống")
+    @Size(max = 50, min= 1 , message = "Không quá 50 kí tự")
     private String title;
     @Column(name = "Author")
+    @NotEmpty(message = "Không để trống")
+    @Size(max = 50, min= 1 , message = "Không quá 50 kí tự")
     private String author;
     @Column(name= "Image")
     private String image;
+
     @Column(name = "Price")
+    @NotNull(message = "Không để trống")
     private Double price;
+
 
 }
